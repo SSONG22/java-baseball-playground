62f922f098075b54ee2e345b8ebd5f8e4df17199
SONG22 <ansongi527@gmail.com>
2021-06-29 14:16:10 +0900

    feat(baseball): 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,31 @@
 package baseball;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(7,8,9), Arrays.asList(1,2,3));
-        System.out.println(result); //
+        NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> computer = numberGenerator.createRandomNumbers();
+        String result = "";
+        while (!result.equals("0 볼 3 스트라이크")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static List<Integer> askNumbers(){
+        System.out.print("숫자를 입력해주세요 :");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for(String number : input.split("")){
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
